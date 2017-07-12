d665692255b754485d23c6ab804584324d8acd17
Bill Calkins <bill@calkinsconsulting.com>
2017-07-12 06:27:19 -0500

    Added date selector for start and end dates.

--- CapCalc/Controllers/SprintDateSelector.swift
+++ CapCalc/Controllers/SprintDateSelector.swift
@@ -12,9 +12,30 @@ import EventKit
 class SprintDateSelector: UIViewController {
     
     var selectedCalendar : EKCalendar?
+    
+    @IBOutlet weak var startDate: UIButton!
+    @IBOutlet weak var endDate: UIButton!
+    @IBOutlet weak var dateSelector: UIDatePicker!
+    
+    var selectedFromStart = false
+    private var selectedStartDate : Date?
+    private var selectedEndDate : Date?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //get changes from the date picker
+        self.dateSelector.addTarget(self, action: #selector(SprintDateSelector.displayDateSelected(sender:)), for: .valueChanged)
+        
+        //set the min and max dates for the date picker
+        if let datePicker = self.dateSelector {
+            //Assume two week sprints
+            datePicker.minimumDate = Date().addDays(numberOfDate: -1)
+            datePicker.maximumDate = Date().addDays(numberOfDate: 14)
+            
+            self.selectedStartDate = datePicker.minimumDate
+            self.selectedEndDate = datePicker.maximumDate
+        }
 
         
     }
@@ -23,34 +44,54 @@ class SprintDateSelector: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func startDateSelected(_ sender: UIButton) {
+        self.selectedFromStart = true
+        if self.dateSelector.isHidden {
+            self.dateSelector.isHidden = false
+        }
+    }
     
-    fileprivate func findWorkCalendar() {
+    @IBAction func endDateSelected(_ sender: UIButton) {
+        self.selectedFromStart = false
+        if self.dateSelector.isHidden {
+            self.dateSelector.isHidden = false
+        }
+    }
+    
+    func displayDateSelected(sender:UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        if self.selectedFromStart {
+            //update the start date button
+            self.startDate.setTitle(dateFormatter.string(from: sender.date), for: .normal)
+            self.selectedStartDate = sender.date
+        } else {
+            //update the end date button
+            self.endDate.setTitle(dateFormatter.string(from: sender.date), for: .normal)
+            self.selectedEndDate = sender.date
+        }
+        
+        
+    }
+    
+    
+    @IBAction func calculateCapacity(_ sender: UIButton) {
         
         guard let givenCalendar = self.selectedCalendar else {
             return
         }
         
-//        var workEvents: [EKEvent]?
-//        
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd"
-//        
-//        //create start and end dates
-//        if let startDate = dateFormatter.date(from: "2017-07-05"),
-//            let endDate = dateFormatter.date(from: "2017-07-12"),
-//            let workCalendar = eventStore.calendar(withIdentifier: "Work") {
-//            
-//            let eventPredicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: [workCalendar])
-//            
-//            workEvents = eventStore.events(matching: eventPredicate)
-//            if let returnedEvents = workEvents {
-//                print("workEvent count = \(returnedEvents.count)")
-//            }
-//        }
+        var workEvents: [EKEvent]?
         
-        
+        if let givenStartDate = self.selectedStartDate, let givenEndDate = self.selectedEndDate {
+            let eventPredicate = EKEventStore().predicateForEvents(withStart: givenStartDate, end: givenEndDate, calendars: [givenCalendar])
+            let workEvents = EKEventStore().events(matching: eventPredicate)
+            print("Number of event: \(workEvents.count)")
+        }
     }
-    
     
     
     
@@ -62,4 +103,14 @@ class SprintDateSelector: UIViewController {
         // Pass the selected object to the new view controller.
     }
 
+}
+
+extension Date {
+    func addDays(numberOfDate: Int) -> Date? {
+        return Calendar.current.date(byAdding: .day, value: numberOfDate, to: self)
+    }
+    
+    func subtractDays(numberOfDays: Int) -> Date? {
+        return Calendar.current.date(byAdding: .day, value: numberOfDays, to: self)
+    }
 }
